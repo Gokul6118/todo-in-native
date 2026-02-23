dd3e504575dbda105d40861d811d2194ce4c9013
gokul-webzenith <gokul@webzenith.tech>
2026-02-23 23:30:25 +0530

    first commit

--- my-app/api/index.ts
+++ my-app/api/index.ts
@@ -1,4 +1,4 @@
 export default async function handler(req: any, res: any) {
-  const { default: app } = await import("../packages/server/dist/index.js");
+  const { default: app } = await import("../packages/server/dist/index.cjs");
   return app(req, res);
 }
